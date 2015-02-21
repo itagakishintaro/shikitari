e492f0a63c6518124dd95f1005c1154fb1be0e78
itagaki <itagaki.shintaro@gmail.com>
2015-02-21 14:30:27 +0900

    Add Important button

--- js/map.js
+++ js/map.js
@@ -4,20 +4,27 @@ var FLOOR_LEVEL = [1, 2];
 var FONT_SIZE = 12;
 var RADIUS = 10;
 
+var floor_data = [];
+
 d3.csv("data/Shibuya_Point.csv", function(error, data) {
     if (error != null) {
         console.log(err);
         return;
     }
+    floor_data[0] = data;
+    floor_data[1] = data;
+    draw();
+});
 
-    var floor_data = [];
-    FLOOR_LEVEL.forEach(function(floor_level){
+function draw(){
+    // var floor_data = [];
+    FLOOR_LEVEL.forEach(function(floor_level, index){
         // 生活者/旅行者ごとにデータを生成
         // floor_data[floor_level] = data.filter(function(element) {
         //     return element.floorLevel === String(floor_level);
         // });
-        floor_data[0] = data;
-        floor_data[1] = data;
+        // floor_data[0] = data;
+        // floor_data[1] = data;
 console.log(floor_data);
         // svgタグを生成
         var svg = d3.select("#floor_" + floor_level)
@@ -42,7 +49,8 @@ console.log(floor_data);
             // ポイントごとにクリックイベントを生成
             .on("click", function(d){ 
                 $('.modal-body').text(d.content);
-                console.log(d);
+                floor_data[index][0].size = Number(floor_data[index][0].size) + 1;
+                console.log(floor_data[0][index].size);
                 // selected_points.push(d);
                 // console.log(selected_points); 
             });
@@ -69,4 +77,4 @@ console.log(floor_data);
         //         'dominant-baseline': 'middle'
         //     });
     });
-});
+}
